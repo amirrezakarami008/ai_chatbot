29ad4001feddd420259249804515100bb41cf777
amirreza <amirrezakarami008@gmail.com>
2025-04-23 19:33:01 +0330

    debug

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-red-500">Main Page</h1>
+      <h1 className="bg-red-500 text-white p-4">Main Page</h1>
     </>
   );
 }
