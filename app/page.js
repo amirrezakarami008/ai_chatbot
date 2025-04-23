929c0338a2efc31df1d184bf07ffa76fd913c18d
amirreza <amirrezakarami008@gmail.com>
2025-04-23 20:35:34 +0330

    update

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import { Button } from "react-bootstrap";
 export default function Home() {
   return (
     <>
-      <h1 className="bg-red-500 text-white p-4">Main Page</h1>
+      <h1 style={{ color: 'var(--primary-color)' }}>Main Page</h1>
       <Button variant="success">کلیک کن</Button>
     </>
   );
