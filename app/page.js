1e6657910267a11e1b7156f0dec3e70f8fba8c83
amirreza <amirrezakarami008@gmail.com>
2025-04-23 20:09:23 +0330

    update

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
+'use client';
 export default function Home() {
   return (
     <>
       <h1 className="bg-red-500 text-white p-4">Main Page</h1>
+      <Button variant="success">کلیک کن</Button>
     </>
   );
 }
